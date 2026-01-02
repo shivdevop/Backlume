f72c797ec8dfbfe27205744e3b1d4abfc1b0b789
shivam <shivamagarwalla05@gmail.com>
2026-01-02 17:57:22 +0530

    added db service for making queries related to creating new user and finding existing

---
+++ src/db/user.queries.js
@@ -0,0 +1,16 @@
+import {pool} from "../config/db.js"
+
+export const createUser=async(email,hashedPassword)=>{
+
+    const {rows}=await pool.query(`
+        INSERT INTO users (email,password_hash) VALUES ($1,$2) RETURNING id,email,is_verifies,created_at`,[email,hashedPassword])
+
+    return rows[0]
+}
+
+export const findUserByEmail=async(email)=>{
+    const {rows}=await pool.query(`
+        SELECT * from users where email=$1`,[email])
+
+    return rows[0]    
+}
